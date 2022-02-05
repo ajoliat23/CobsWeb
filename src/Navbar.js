0fd52c43a42f8b8be9bd18b9468e2f1436261193
ajoliat23 <62565033+ajoliat23@users.noreply.github.com>
2022-02-04 21:32:31 -0500

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-
+//Also got this Navbar stuff from The Net Ninja
 const Navbar = () => {
     return ( 
         <nav className = "navbar"> 
