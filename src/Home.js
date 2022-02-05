8c9645b5c87d64f7d938cae5c0c77aa168f6b85b
ajoliat23 <62565033+ajoliat23@users.noreply.github.com>
2022-02-04 20:42:40 -0500

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -6,10 +6,10 @@ const Home = () => {
             <h2>We have all your shoe needs!</h2>
             <p>For just three easy payments of $2.22 a week you can have a brand new pair of shoes every week! 
                 Simply continue to one of the pages above to accept our terms and conditions!</p>
-            <img src = "images/mensdress.jpg" alt="Men's dress shoes" height = "140px" width = "350px"></img>
-            <img src = "images/gymshoes.jpg" alt="Gym shoes"></img>
-            <img src = "images/sandals.jpg" alt="Sandals"></img>
-            <img src = "images/docs.jpg" alt="Doc Martens" ></img>
+            <img src = "mensdress.jpg" alt="Men's dress shoes" height = "140px" width = "350px"></img>
+            <img src = "gymshoes.jpg" alt="Gym shoes"></img>
+            <img src = "sandals.jpg" alt="Sandals"></img>
+            <img src = "docs.jpg" alt="Doc Martens" ></img>
 
         </div>
      );
