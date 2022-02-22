938b89f1def8b4554a071cadfad009d7d01816ef
ajoliat23 <62565033+ajoliat23@users.noreply.github.com>
2022-02-22 11:25:41 -0500

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -3,13 +3,44 @@ import React from 'react';
 const Home = () => {
     return ( 
         <div className="home">
-            <h2>We have all your shoe needs!</h2>
-            <p>For just three easy payments of $2.22 a week you can have a brand new pair of shoes every week! 
-                Simply continue to one of the pages above to accept our terms and conditions!</p>
-            <img src = "mensdress.jpg" alt="Men's dress shoes" height = "140px" width = "350px"></img>
-            <img src = "gymshoes.jpg" alt="Gym shoes"></img>
-            <img src = "sandals.jpg" alt="Sandals"></img>
-            <img src = "docs.jpg" alt="Doc Martens" ></img>
+            <h2>We have all your Sole needs!</h2>
+            <center>
+        <table>
+            <tr>  
+                <td> 
+                    <center>
+                        
+                       <img class="img2" src = "/sandSole.jpeg" alt="Dover Sole" ></img>
+                       
+                    </center>
+                    
+                </td>
+                <td> 
+                    <center>
+                      <img class="img2" src = "/GreySole.jpg" alt="Dover Sole"></img>
+                     
+                    </center>
+                    
+                </td>
+            </tr>
+            <tr>  
+                <td> 
+                    <center>
+                        <img class="img2" src = "/greenSole.jpeg" alt="Dover Sole" ></img>
+                    </center>
+                       
+                </td>
+                <td> 
+                    <center>
+                       <img class="img2" src = "/southernSole.jpeg" alt="Dover Sole" ></img>
+                    </center>
+                    
+                </td>
+            </tr>
+        
+        </table>
+    </center>
+            
 
         </div>
      );
