eb69511a4d1ee95d77166a3a0c4f2ecf79197c78
ajoliat23 <62565033+ajoliat23@users.noreply.github.com>
2022-02-22 21:35:02 -0500

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -3,27 +3,24 @@ import React from 'react';
 const Home = () => {
     return ( 
         <div className="home">
-            <h2>We have all your Sole needs!</h2>
-            <center>
+            <h2 className='white'>We have all your Sole needs!</h2>
+            <p className='white'>Continue to either the Dark Page or Light Page to see our selection!</p>
+        <center>
         <table>
             <tr>  
                 <td> 
                     <center>
-                        
-                       <img class="img2" src = "/public/sandSole.jpeg" alt="Sand Sole" ></img>
-                       
+                       <img class="img2" src = "/sandSole.jpeg" alt="Sand Sole" ></img>
                     </center>
                     
                 </td>
                 <td> 
                     <center>
-                      <img class="img2" src = "/GreySole.jpg" alt="Grey Sole"></img>
+                      <img class="img2" src = "/GreySole.jpg" alt="Gray Sole"></img>
                      
                     </center>
                     
-                </td>
-            </tr>
-            <tr>  
+                </td>  
                 <td> 
                     <center>
                         <img class="img2" src = "/greenSole.jpeg" alt="Green Sole" ></img>
