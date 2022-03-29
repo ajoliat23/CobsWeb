ef33fe0709186e1544b5ea56ca06d920f1a81040
ajoliat23 <62565033+ajoliat23@users.noreply.github.com>
2022-03-28 21:41:23 -0400

    Create StatusBar.js

---
+++ src/components/StatusBar.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+
+const StatusBar = () => { 
+
+    
+    return ( 
+        <html>
+            <div className='status'>
+                <h1>Login Bonus</h1>
+                <p>Every Log in = <b className='sale'>FREE</b> bronze Cob Buck!</p>
+                
+                <img src = 'Done.png' height='100px'></img>
+                
+                <p>You have unlocked dark mode! Earn more badges for more achievements!</p>
+            </div>
+        </html>
+    );
+}
+ 
+export default StatusBar;
