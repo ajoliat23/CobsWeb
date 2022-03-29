2d316292e5b6546188b5828adb8bdca24a31c779
ajoliat23 <62565033+ajoliat23@users.noreply.github.com>
2022-03-29 14:01:50 -0400

    Update Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,7 +7,15 @@ export default function Product(props){
             <p><strong className='sale'>SALE!!</strong></p> 
             <img className='small' src={product.image} alt = {product.name}></img>
             <h3>{product.name}</h3>
-            <div><div className="old"><img className = 'tiny' src='bronze.png'></img>: {product.price1 + 2} <img src='silver.png' className = 'tiny'></img>: {product.price2 + 2}</div><strong id="larger">Only <img className = 'tiny' src='bronze.png'></img>:{product.price1} <img className = 'tiny' src='silver.png'></img>: {product.price2 + 2}!</strong></div>
+            <div>
+                <div className="old"><img className = 'tiny' src='bronze.png'></img>: {product.price1 + 2} 
+                <img src='silver.png' className = 'tiny'></img>: {product.price2 + 2}</div>
+            
+                <strong id="larger">Only <img className = 'tiny' src='bronze.png'></img>:{product.price1} 
+                <img className = 'tiny' src='silver.png'></img>: {product.price2}!</strong>
+                
+            </div>
+
             <div>
                 <button onClick={() => onAdd(product)}>Add To Cart</button>
             </div>
