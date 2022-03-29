7a56632e91c8af099460d16e53b8d001be1b6ff0
ajoliat23 <62565033+ajoliat23@users.noreply.github.com>
2022-03-28 21:47:48 -0400

    Create TemplateL.js

---
+++ src/TemplateL.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import './index.css';
+import Game from './components/GameL';
+import Nav2 from './components/Nav2';
+import StatusBar from './components/StatusBar';
+import Store from './StoreL';
+import LoginL from './components/LoginL';
+import Thanks from './components/Thanks';
+import HomeL from './HomeL';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+function templateL() { //all this routing stuff i learned from The Net Ninja
+   
+    return (
+        <main>
+            <Router>
+      <div className ="App">
+       
+        <HomeL/>
+        <div className = "content"> 
+          <Switch> 
+
+            <Route path = "/game">
+              <Game />
+            </Route>
+            <Route path = "/store">
+              <Store />
+            </Route>
+            <Route path = "/loginL">
+              <LoginL />
+            </Route>
+            <Route path = "/thanks">
+              <Thanks />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+
+    
+        </main>
+    );
+}
+
+export default templateL;
