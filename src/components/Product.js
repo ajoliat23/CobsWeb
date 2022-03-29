813d84814825b6ba819e0520430ffa1accfecdfa
ajoliat23 <62565033+ajoliat23@users.noreply.github.com>
2022-03-28 21:39:43 -0400

    Update Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,5 @@
 import React from 'react'
 //You guessed it: Coding with Basir 'inspired' this--his tutorial was extremely helpful and only 45 minutes
-
 export default function Product(props){
     const {product, onAdd} = props;
     return(
@@ -8,7 +7,7 @@ export default function Product(props){
             <p><strong className='sale'>SALE!!</strong></p> 
             <img className='small' src={product.image} alt = {product.name}></img>
             <h3>{product.name}</h3>
-            <div><div className="old">${product.price + 25}</div><strong id="larger">Only ${product.price}!</strong></div>
+            <div><div className="old"><img className = 'tiny' src='bronze.png'></img>: {product.price1 + 2} <img src='silver.png' className = 'tiny'></img>: {product.price2 + 2}</div><strong id="larger">Only <img className = 'tiny' src='bronze.png'></img>:{product.price1} <img className = 'tiny' src='silver.png'></img>: {product.price2 + 2}!</strong></div>
             <div>
                 <button onClick={() => onAdd(product)}>Add To Cart</button>
             </div>
