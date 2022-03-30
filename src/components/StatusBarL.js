7eb8bfdde99b399db7bb4e01cf56c127e6db7566
ajoliat23 <62565033+ajoliat23@users.noreply.github.com>
2022-03-29 22:35:53 -0400

    Create StatusBarL.js

---
+++ src/components/StatusBarL.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+
+const StatusBar = () => { 
+
+    
+    return ( 
+        <html>
+            <div className='status'>
+                <h1>Login Bonus</h1>
+                <p>Every Log in = 1 bronze Cob Buck!</p>
+                
+                <img src = 'DoneL.png' height='100px'></img>
+                
+            </div>
+        </html>
+    );
+}
+ 
+export default StatusBar;
