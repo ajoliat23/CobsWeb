7efd829de3dd2787cc89cbfe96bad3ac9a69d6ee
ajoliat23 <62565033+ajoliat23@users.noreply.github.com>
2022-03-29 22:34:24 -0400

    Update Nav2.js

--- src/components/Nav2.js
+++ src/components/Nav2.js
@@ -2,16 +2,14 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const Nav2 = () => { //got this navbar stuff also from The Net Ninja
-    const handleRecharge = () => {
-        alert('Recharged CobBucks for $20');
-    }
+    
     return ( 
         <nav > 
             <div className="nav2">
                 <Link to="/login" ></Link>
                 <Link to="/game" className='space'>Game</Link>
                 <Link to="/store" className='space'>Store</Link>
-                <Link to="/recharge" className='space' onClick={()=>handleRecharge()}>Recharge</Link>
+                <Link to="/recharge" className='space' >Recharge</Link>
             </div>
         </nav>
     );
