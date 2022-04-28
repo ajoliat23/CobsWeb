5de885c76b8d490fc5f19ee6d9284c73cab0a324
ajoliat23 <62565033+ajoliat23@users.noreply.github.com>
2022-04-28 17:16:38 -0400

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,11 +4,11 @@ import {Link} from 'react-router-dom';
 const Navbar = () => { //got this navbar stuff also from The Net Ninja
     return ( 
         <nav className = "navbar"> 
-            <h1>Cob's Soulo Game</h1>
+            <h1>Cob's Soul Music</h1>
             <div className="links">
                 <Link to="/"></Link>
-                <Link to="/template">Dark Page</Link>
-                <Link to="/templateL">Light Page</Link>
+                <Link to="/darkpage">Dark Page</Link>
+                <Link to="/lightpage">Light Page</Link>
                 <Link to = "/terms"></Link>
                 <Link to = "/thanks"></Link>
             </div>
